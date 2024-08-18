73d38739e9868b654719a637416675279ef5be37
yanfi-sher <loss.soul20@gmail.com>
2024-08-18 22:31:53 +0300

    HW 19, stream

--- src/main/java/telran/stream/StreamTasks.java
+++ src/main/java/telran/stream/StreamTasks.java
@@ -3,15 +3,13 @@ package telran.stream;
 import java.util.Random;
 
 public class StreamTasks {
-    public static int[] shuffle(int [] arr) {
-        Random random = new Random();
-        for (int i = arr.length - 1; i > 0; i--) {
-            int randomIndexToSwap = random.nextInt(arr.length);
-            int temp = arr[i];
-            arr[i] = arr[randomIndexToSwap];
-            arr[randomIndexToSwap] = temp;
-        }
-        return arr;
+    public static int[] shuffle(int[] arr) {
+        return new Random()
+            .ints(0, arr.length)
+            .distinct()
+            .limit(arr.length)
+            .map(i -> arr[i])
+            .toArray();
     }
     
 }
--- src/test/java/telran/stream/StreamTasksTest.java
+++ src/test/java/telran/stream/StreamTasksTest.java
@@ -1,7 +1,9 @@
 package telran.stream;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -9,15 +11,11 @@ public class StreamTasksTest {
 
     @Test
     void testShuffle() {
-        int[] arr = {1, 2, 3, 4, 5};
-        int[] originalArr = arr.clone();
-
-        StreamTasks.shuffle(arr);
-        boolean isShuffled = !Arrays.equals(originalArr, arr);
-        boolean hasSameElements = Arrays.equals(originalArr, Arrays.stream(arr).sorted().toArray());
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int[] newArr = StreamTasks.shuffle(arr);
         
-        assertTrue(isShuffled);
-        assertTrue(hasSameElements);
+        assertTrue(IntStream.range(0, arr.length).anyMatch(i -> newArr[i] != arr[i]));
+        assertArrayEquals(Arrays.stream(arr).sorted().toArray(), Arrays.stream(newArr).sorted().toArray());
     }
 
 }
